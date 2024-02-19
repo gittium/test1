5b70258fe30a5c4ee01451ce7a57f79a0564b53a
gittium pookiebear <umcode123a@gmail.com>
2024-02-19 17:10:07 +0000

    deplot testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,26 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+st.title('🤫🧏 Iris flower')
+# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv')
+# st.write(df)
+st.sidebar.subheader('Enter your age')
+value = st.sidebar.text_input('')
 
-st.title('🎈 App Name')
+if value:
+    st.subheader(f"You're {value} yo")
+    st.warning(value)
+    rdata = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv')
+    st.write(rdata)
+    rdata2 = rdata.groupby('Species').mean()
+    st.write(rdata2)
+    
+    st.scatter_chart(rdata2)
+    
 
-st.write('Hello world!')
+
+else:
+    st.subheader('👈 Please enter your age')
+    st.error('i will guess your age!')
+    
